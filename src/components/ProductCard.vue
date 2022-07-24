6ab88a95edd34e86c21977e2f30739fe8fb1eb8d
Bynood <quent1_jarry@hotmail.com>
2022-07-24 20:51:50 +0200

    feat ProductCard and CardsGrid functionnal -> need their fince-tuned CSS

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="card col-sm-3">
+    <img v-bind:src="obj.img_url" class="card-img-top">
+    <div class="card-body">
+        <h3 class="card-title"> {{ this.obj.name }} </h3>
+        <p class="card-text"> {{ this.obj.description }} </p>
+        <h5 class="card-price"> {{ this.obj.price }}€ </h5>
+        <div class="discover-btn-container">
+          <a href="#" class="btn btn-outline-secondary btn-discover"> Découvrir </a>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default{
+    name: "ProductCard",
+    props: { //Not included in data (as we did in HTML) because parent vue (CardsGrid in that case) will feed the information
+        obj: Object,
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
+
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,18 +3,21 @@
   <!-- <div class="home">
      <HelloWorld msg="Welcome to Your Vue.js App" /> 
   </div> -->
+  <CardsGrid  amount="4" category="all" />
 </template>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from "@/components/HelloWorld.vue";
 import NavBar from "@/components/NavBar.vue";
+import CardsGrid from "../components/CardsGrid.vue";
 
 export default {
   name: "HomeView",
   components: {
     // HelloWorld,
-    NavBar
+    NavBar,
+    CardsGrid
 },
 };
 </script>
